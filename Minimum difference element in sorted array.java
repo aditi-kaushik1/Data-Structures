9d9e7c40d0153253520cebc9aa2ebb937521a7c3
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-04-12 22:51:35 +0530

    Update Minimum difference element in sorted array.java

--- Minimum difference element in sorted array.java
+++ Minimum difference element in sorted array.java
@@ -6,6 +6,8 @@
         System.out.println(minimumDifferenceElement(arr,12));
     }
     
+    //Solution 1
+    //Shorter Method
     public static int minimumDifferenceElement(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
@@ -26,3 +28,59 @@
             return arr[left];
     }
 }
+
+//Solution 2
+//Find ceil and floor and then calculate absolute differences.
+//public class Main {
+//     public static void main(String[] args) {
+//         System.out.println(minimumDifferenceElement(arr,8));
+//     }
+    
+//     public static int minimumDifferenceElement(int[] arr, int target) {
+//         int floor = floor(arr,target);
+//         int ceil = ceil(arr,target);
+//         if(Math.abs(ceil - target) > Math.abs(floor - target))
+//             return floor;
+//         else
+//             return ceil;
+//     }
+    
+//     static int[] arr = new int[]{1,2,3,4,5,6,9,10};
+//     static int left = 0;
+//     static int right = arr.length - 1;
+//     static int mid;
+    
+//     public static int ceil(int[] arr, int target) {
+//         int res = -1;
+//         left = 0;
+//         right = arr.length - 1;
+//         while(left <= right) {
+//             mid = left + (right - left)/2;
+//             if(arr[mid] == target)
+//                 return target;
+//             else if(target < arr[mid]) {
+//                 res = arr[mid];
+//                 right = mid - 1;
+//             }
+//             else
+//                 left = mid + 1;
+//         }
+//         return res;
+//     }
+    
+//     public static int floor(int[] arr, int target) {
+//         int res = -1;
+//         while(left <= right) {
+//             mid = left + (right - left)/2;
+//             if(arr[mid] == target)
+//                 return target;
+//             else if(target > arr[mid]) {
+//                 res = arr[mid];
+//                 left = mid + 1;
+//             }
+//             else
+//                 right = mid - 1;
+//         }
+//         return res;
+//     }
+// }
