aa1c8bd753b46ef5edcb904167bb0a563e8616b9
aditi-kaushik1 <47269969+aditi-kaushik1@users.noreply.github.com>
2021-04-22 22:26:15 +0530

    Create Min Stack.java

---
+++ Min Stack.java
@@ -0,0 +1,66 @@
+/* Design a data-structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull() and an additional operation getMin() which should return minimum element from the SpecialStack. Your task is to complete all the functions, using stack data-Structure.
+
+
+Example 1:
+
+Input:
+Stack: 18 19 29 15 16
+Output: 15
+Explanation:
+The minimum element of the stack is 15.
+ 
+
+
+Your Task:
+Since this is a function problem, you don't need to take inputs. You just have to complete 5 functions, push() which takes the stack and an integer x as input and pushes it into the stack; pop() which takes the stack as input and pops out the topmost element from the stack; isEmpty() which takes the stack as input and returns true/false depending upon whether the stack is empty or not; isFull() which takes the stack and the size of the stack as input and returns true/false depending upon whether the stack is full or not (depending upon the
+given size); getMin() which takes the stack as input and returns the minimum element of the stack. 
+Note: The output of the code will be the value returned by getMin() function.
+
+
+Expected Time Complexity: O(N) for getMin, O(1) for remaining all 4 functions.
+Expected Auxiliary Space: O(1) for all the 5 functions.
+
+
+Constraints:
+1 ≤ N ≤ 104 */
+
+/*Complete the function(s) below*/
+class GfG{
+    Stack<Integer> stackSupporter = new Stack<>();
+    int count = 0;
+	public void push(int a,Stack<Integer> s)
+	{
+	    count++;;
+	    if(stackSupporter.isEmpty() || stackSupporter.peek() >= a)
+	        stackSupporter.push(a);
+	    s.push(a);
+	}
+	public int pop(Stack<Integer> s)
+    {
+        if(s.isEmpty())
+            return -1;
+        count--;
+        if(stackSupporter.peek().equals(s.peek()))
+            stackSupporter.pop();
+        return s.pop();
+	}
+	public int min(Stack<Integer> s)
+    {
+        if(stackSupporter.isEmpty())
+            return -1;
+        return stackSupporter.peek();
+	}
+	public boolean isFull(Stack<Integer>s, int n)
+    {
+        if(count == n)
+            return true;
+            return false;
+	}
+	public boolean isEmpty(Stack<Integer>s)
+        {
+        if(s.isEmpty())
+        return true;
+        else
+        return false;
+	}
+}
