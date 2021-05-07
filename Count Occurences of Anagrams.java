53b80a9de683987153617f4fecc13f8a3166541c
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-05-08 00:59:16 +0530

    Update Count Occurences of Anagrams.java

--- Count Occurences of Anagrams.java
+++ Count Occurences of Anagrams.java
@@ -23,6 +23,57 @@ Constraints:
 1 <= |pat| <= |txt| <= 105
 Both string contains lowercase english letters. */
 
+//Solution 1
+//Efficient solution
+//O(1) space and O(n) time used
+//Use int[] instead of HashMaps.
+class Solution {
+
+    int search(String pat, String txt) {
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        int k = pat.length();
+        char[] arr = new char[26];
+        int count = 0;
+        
+        for(char c : pat.toCharArray()) {
+            if(arr[c - 'a'] == 0)
+                count++;
+            arr[c - 'a']++;
+        }
+        
+        while(j < txt.length()) {
+            if(pat.indexOf(txt.charAt(j)) != -1) {
+                arr[txt.charAt(j) - 'a']--;
+                if(arr[txt.charAt(j) - 'a'] == 0)
+                    count--;
+            }
+            
+            if(j - i + 1 < k)
+                j++;
+                
+            else {
+                if(count == 0)
+                    ans++;
+                if(pat.indexOf(txt.charAt(i)) != -1) {
+                    arr[txt.charAt(i) - 'a']++;
+                    if(arr[txt.charAt(i) - 'a'] == 1)
+                        count++;
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
+
+//Solution 2
+//HashMap used
+//O(n) space used
+//Inefficient solution
+//Whenever it is given that the string contains only lower case alphabets, apply int[] to store the occurences of characters in string.
 class Solution {
 
     int search(String pat, String txt) {
