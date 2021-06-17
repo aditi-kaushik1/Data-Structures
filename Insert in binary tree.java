f82438925b933007c9573536c1dcd9e81ca77f02
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-06-17 12:43:16 +0530

    Create Insert in binary tree.java

---
+++ Insert in binary tree.java
@@ -0,0 +1,25 @@
+	public class Main {
+    public static void insert(int data) {
+	    Queue<Node> = new LinkedList<>();
+	    if(root == null) {
+	        root = new Node(data);
+	        return;
+	    }
+	    q.add(root);
+	    while(!q.isEmpty()) {
+	        Node temp = q.pop();
+	        if(temp.left == null) {
+	            temp.left = new Node(data);
+	            return;
+	        }
+	        else
+	            q.add(temp.left);
+	        if(temp.right == null) {
+	            temp.right = new Node(data);
+	            return;
+	        }
+	        else
+	            q.add(temp.right);
+	    }
+	  }
+  }
