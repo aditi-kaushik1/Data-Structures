2e58e2179c499ea8557f6339e8f9e52fb497b0fd
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-07-05 10:04:47 +0530

    Update Insert in binary tree.java

--- Insert in binary tree.java
+++ Insert in binary tree.java
@@ -1,25 +1,39 @@
-	public class Main {
+public class Main {
     public static void insert(int data) {
-	    Queue<Node> = new LinkedList<>();
-	    if(root == null) {
-	        root = new Node(data);
-	        return;
-	    }
-	    q.add(root);
-	    while(!q.isEmpty()) {
-	        Node temp = q.pop();
-	        if(temp.left == null) {
-	            temp.left = new Node(data);
-	            return;
-	        }
-	        else
-	            q.add(temp.left);
-	        if(temp.right == null) {
-	            temp.right = new Node(data);
-	            return;
-	        }
-	        else
-	            q.add(temp.right);
-	    }
-	  }
-  }
+	    //Iterative method
+// 	    Queue<Node> = new LinkedList<>();
+// 	    if(root == null) {
+// 	        root = new Node(data);
+// 	        return;
+// 	    }
+// 	    q.add(root);
+// 	    while(!q.isEmpty()) {
+// 	        Node temp = q.pop();
+// 	        if(temp.left == null) {
+// 	            temp.left = new Node(data);
+// 	            return;
+// 	        }
+// 	        else
+// 	            q.add(temp.left);
+// 	        if(temp.right == null) {
+// 	            temp.right = new Node(data);
+// 	            return;
+// 	        }
+// 	        else
+// 	            q.add(temp.right);
+// 	    }
+	    
+	   //Recursive method
+	   root = helper(root, data);
+    }
+	public TreeNode helper(TreeNode node, val) {
+		if(node == null)
+			return new TreeNode(val);
+		if(val < node.left)
+			node.left = helper(node.left, val);
+		else
+			node.right = helper(node.right);
+		return node;
+		
+	}
+}
