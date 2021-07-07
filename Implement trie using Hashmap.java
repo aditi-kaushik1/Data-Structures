5faa34538685ff992a16e68aec389a90e798bb77
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-07-07 11:30:42 +0530

    Create Implement trie using Hashmap.java

---
+++ Implement trie using Hashmap.java
@@ -0,0 +1,59 @@
+//Trie class
+
+import java.util.HashMap;
+
+public class Trie {
+    public static int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private char value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node (char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + value;
+        }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            children.get(ch);
+        }
+    }
+    public Node root = new Node(' ');
+
+    public void insert(String word)  {
+        Node current = root;
+        for(var ch : word.toCharArray()) {
+            if(!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
+        }
+        current.isEndOfWord = true;
+    }
+}
+
+
+//Main class
+
+import java .util.*;
+public class Main
+{
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("cat");
+        trie.insert("can");
+        System.out.println("Done");
+    }
+}
