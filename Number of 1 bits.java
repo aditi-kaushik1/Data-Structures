f8f210b34dcd9e8c73999d304e1e919463d07d22
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-08-24 22:37:48 +0530

    Update Number of 1 bits.java

--- Number of 1 bits.java
+++ Number of 1 bits.java
@@ -1,11 +1,20 @@
 class Solution {
     static int setBits(int N) {
+        //Solution 1
+        // int count = 0;
+        // for(int i = 0; i < 32; i++) {
+        //     if(((N >> i) & 1) == 1)
+        //         count++;
+        // }
+        // return count;
+        
+        //Solution 2
         int count = 0;
         while(N != 0) {
             int x = N & 1;
-            if(x == 1) 
+            if(x == 1)
                 count++;
-            N = N >> 1;
+            N >>= 1;
         }
         return count;
     }
